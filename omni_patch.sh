95588598352aeddac9cd3a70989fd9d9e4c3112b
MrGezz <MrGezz@users.noreply.github.com>
2014-04-12 16:38:56 +0800

    Delete omni_patch.sh

--- omni_patch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Patch by MrGezz
-
-DIR=$(pwd)
-
-cd $DIR/build/
-git fetch https://github.com/deedwar/platform_build.git beanstalk-kitkat && git cherry-pick c1eab3d111e49d8fc44749d81e7f5610a4443fd3
-
